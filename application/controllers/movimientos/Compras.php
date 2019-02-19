738c52f7dec9ef746ecc54f147b9a9b76f969b72
Konny <pinchehugochan@gmail.com>
2019-02-18 20:31:00 -0600

    proveedores

--- application/controllers/movimientos/Compras.php
+++ application/controllers/movimientos/Compras.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Compras extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
-		$this->load->model("Compras_model");
+        $this->load->model("Compras_model");
 	}
 
 	public function index(){
@@ -24,4 +24,15 @@ class Compras extends CI_Controller {
         $this->load->view("layouts/footer");
     }
 
+    public function getProveedores(){
+        $valorProveedor = $this->input->post("empresa");
+		$prov = $this->Compras_model->getProveedores($valorProveedor);
+		echo json_encode($prov);
+    }
+
+    public function getProductos(){
+        $valor = $this->input->post("valor");
+		$producto = $this->Reabastecer_model->getProductos($valor);
+		echo json_encode($producto);
+    }
 }
