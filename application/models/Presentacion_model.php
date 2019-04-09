09c5b95a0caf5c483af29f775c3bd5842323fe67
Konny <pinchehugochan@gmail.com>
2019-04-08 19:42:54 -0600

    arreglando productos

--- application/models/Presentacion_model.php
+++ application/models/Presentacion_model.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Presentacion_model extends CI_Model {
 	
-    public function getPresentaciones(){ 
+  public function getPresentaciones(){ 
 		$resultados = $this->db->get("presentacion");
 			return $resultados->result();
 	}
