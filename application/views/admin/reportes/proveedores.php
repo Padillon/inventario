78bbb34970d0ec5af4e9a700fd500c80ca5a158c
KonnyLiz <pinchehugochan@gmail.com>
2019-07-08 16:26:14 -0600

    pdf

--- application/views/admin/reportes/proveedores.php
+++ application/views/admin/reportes/proveedores.php
@@ -1,12 +1,22 @@
 <?php
-
-
+class Mypdf extends TCPDF {
+    public function Footer(){
+        $this->SetY(-15);
+        $this->SetFont('helvetica', 'I', 8);
+        $this->Cell(0, 10, 'Pagina '.$this->getAliasNumPage().' de '.$this->getAliasNbPages(), 
+        0, false, 'C', 0, '', 0, false, 'T', 'M');
+    }
+}
 
 $cont = 0;
 $link = base_url();
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new Mypdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_RIGHT);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 $pdf->startPageGroup();
+
 $pdf->AddPage();
 $estilos = <<<EOF
     
@@ -93,4 +103,20 @@ EOF;
 $tabla=utf8_encode($tabla);
 $pdf->writeHTML(utf8_decode($tabla), true, false, false, false, '');
 
+
+// $paginador = <<<EOF
+// <table>
+//     <tr>
+//         <td style="width:540px"><img src="images/back.jpg"></td>
+//     </tr>
+// </table>
+// <table>
+// 		<tr>
+// 			<td>Mostrando $cont de </td>
+// 		</tr>
+// 	</table>
+
+// EOF;
+// $paginador=utf8_encode($paginador);
+// $pdf->writeHTML(utf8_decode($paginador), true, false, false, false, '');
 $pdf->Output('factura.pdf', 'I');
